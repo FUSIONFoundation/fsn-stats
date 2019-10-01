0182f049368bbd4e837d4663e0ca5d41011a38c3
Joey Kheireddine <joey.kheireddine@outlook.com>
2019-10-01 13:23:27 -0400

    Added all cases to frontend

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -8,9 +8,72 @@ const client = new W3CWebSocket('ws://127.0.0.1:5000/primus');
 client.onopen = () => {
     console.log('WebSocket Client Connected');
 };
-client.onmessage = (message) => {
-    console.log(message);
+client.onmessage = (data) => {
+    socketAction(data.data)
 };
+
+function socketAction(data)
+{
+    let action = JSON.parse(data).action;
+    let nodeData = JSON.parse(data).data;
+
+    switch(action)
+    {
+        case "init":
+            console.log(action , nodeData);
+            break;
+
+        case "add":
+            console.log(action , nodeData);
+            break;
+
+        case "update":
+            console.log(action , nodeData);
+            break;
+
+        case "block":
+        console.log(action , nodeData);
+        break;
+
+        case "pending":
+            console.log(action , nodeData);
+            break;
+
+        case "stats":
+            console.log(action , nodeData);
+            break;
+
+        case "info":
+            console.log(action , nodeData);
+            break;
+
+        case "blockPropagationChart":
+            console.log(action , nodeData);
+            break;
+
+        case "uncleCount":
+            console.log(action , nodeData);
+            break;
+
+        case "charts":
+            console.log(action , nodeData);
+            break;
+
+        case "inactive":
+            console.log(action , nodeData);
+            break;
+
+        case "latency":
+            console.log(action , nodeData);
+            break;
+
+        case "client-ping":
+            console.log(action , nodeData);
+            break;
+
+    }
+
+}
 
 function App() {
     return (
