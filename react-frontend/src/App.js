b533e1e1a92c35b0c93fb8b67e58a5450fbb8d11
Joey Kheireddine <joey.kheireddine@outlook.com>
2019-10-01 13:03:33 -0400

    Added primus initialization to frontend

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { w3cwebsocket as W3CWebSocket } from "websocket";
 
+const client = new W3CWebSocket('ws://127.0.0.1:5000/primus');
 
+client.onopen = () => {
+    console.log('WebSocket Client Connected');
+};
+client.onmessage = (message) => {
+    console.log(message);
+};
 
 function App() {
     return (
